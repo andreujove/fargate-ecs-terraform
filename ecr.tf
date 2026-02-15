ebdacb0a8170d9288adb5abb65b5c9bf689b9df4
AndreuJove <andreujove@gmail.com>
2026-02-15 16:19:52 +0100

    Fix name ecr

--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,30 @@
 resource "aws_ecr_repository" "ecr_repository" {
-  name                 = var.repository_name
-  image_tag_mutability = var.image_mutability # Usually "MUTABLE" for dev, "IMMUTABLE" for prod
-
-  force_delete = true
+  name                 = "${var.name_prefix}"
+  image_tag_mutability = var.image_mutability
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
   }
-
-  tags = {
-    Name        = var.repository_name
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
 }
+
+# resource "aws_ecr_lifecycle_policy" "main" {
+#   repository = aws_ecr_repository.ecr_repository.name
+
+#   policy = jsonencode({
+#     rules = [
+#       {
+#         rulePriority = 1
+#         description  = "Keep last 10 images"
+#         selection = {
+#           tagStatus     = "any"
+#           countType     = "imageCountMoreThan"
+#           countNumber   = 10
+#         }
+#         action = {
+#           type = "expire"
+#         }
+#       }
+#     ]
+#   })
+# }
