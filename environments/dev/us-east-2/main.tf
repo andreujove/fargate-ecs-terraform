e84a70a376a58fa3f0738e02f07a02f9fd9b3ee4
AndreuJove <andreujove@gmail.com>
2026-03-02 11:52:24 +0100

    Add all improvements for moduled repository

--- environments/dev/us-east-2/main.tf
+++ environments/dev/us-east-2/main.tf
@@ -11,22 +11,22 @@ data "aws_route_table" "default_vpc_rt" {
   }
 }
 
-# Existing IAM Role existing in the exam account: 
-data "aws_iam_role" "task_ecs" {
-  name = "ecsTaskExecutionRole"
-}
-
 data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
-
-
 
 module "ecr" {
   source            = "../../../modules/ecr"
   name_prefix       = var.name_prefix
   environment       = var.environment
   image_mutability  = var.image_mutability
+}
+ 
+module "security" {
+  source            = "../../../modules/security"
+  name_prefix       = var.name_prefix
+  vpc_id            = data.aws_vpc.default.id
+  app_port          = var.app_port
 }
 
 module "network" {
@@ -38,10 +38,34 @@ module "network" {
   aws_region          = data.aws_region.current.name
 }
 
-# module "ecs" {
-#   source                    = "../../../modules/ecs"
-#   name_prefix               = var.name_prefix
-#   app_port                  = var.app_port
-#   aws_region                = data.aws_region.current
-#   task_execution_iam_role   = data.aws_iam_role.task_ecs.arn
-# }
+module "alb" {
+  source              = "../../../modules/alb"
+  depends_on = [
+    module.network,
+    module.security
+  ]
+
+  name_prefix               = var.name_prefix
+  public_subnet_ids         = module.network.public_subnet_ids
+  alb_security_group_id     = module.security.alb_security_group_id
+  vpc_id                    = data.aws_vpc.default.id
+}
+
+module "ecs" {
+  depends_on = [
+    module.network,
+    module.security,
+    module.alb
+  ]
+  source                    = "../../../modules/ecs"
+  name_prefix               = var.name_prefix
+  app_port                  = var.app_port
+  aws_region                = data.aws_region.current.name
+  aws_account_id            = data.aws_caller_identity.current.account_id
+  vpc_id                    = data.aws_vpc.default.id
+  default_vpc_rt_id         = data.aws_route_table.default_vpc_rt.id
+  ecs_tasks_sg_id           = module.security.ecs_tasks_sg_id
+  private_subnet_ids        = module.network.private_subnet_ids
+  aws_lb_target_group_arn   = module.alb.aws_lb_target_group_arn
+  vpc_endpoints_sg_id       = module.security.vpc_endpoints_sg_id
+}
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -12,3 +12,4 @@ output "igw_id" {
   description = "The ID of the Internet Gateway"
   value       = aws_internet_gateway.gw.id
 }
+
